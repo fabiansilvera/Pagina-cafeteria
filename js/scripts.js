6ce327529c2c4e85995f32b134bce959ae11ace6
Fabian Silvera <fabiansilvera@gmail.com>
2022-06-28 07:12:36 -0300

    Validacion de formularios en JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,52 @@
-const btnEnviar = document.querySelector('.boton--primario');
-btnEnviar.addEventListener('click', function() {
-    console.log('enviando formulario')
-});
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formulario');
+
+nombre.addEventListener('input', leertexto);
+email.addEventListener('input', leertexto);
+mensaje.addEventListener('input', leertexto);
+formulario.addEventListener('submit', function(evento){
+    evento.preventDefault();
+
+    const {nombre, email, mensaje } = datos
+
+   if(nombre === '' || email === '' || mensaje === '') {
+    mostrarError('Todos los campos son obligatorios');
+    return;
+   } else {
+    mostrarCorrecto('Mensaje enviado correctamente');
+   };
+}) 
+
+function leertexto(e) {
+    datos[e.target.id] = e.target.value;
+}
+
+function mostrarError(mensaje) {
+    const error = document.createElement('P');
+    error.textContent = mensaje;
+    error.classList.add('error');
+
+    formulario.appendChild (error);
+    setTimeout(()=>{
+        error.remove();
+    }, 2000);
+}
+
+function mostrarCorrecto(mensaje) {
+    const correcto = document.createElement('P');
+    correcto.textContent = mensaje;
+    correcto.classList.add('correcto');
+
+    formulario.appendChild (correcto);
+    setTimeout(()=>{
+        correcto.remove();
+    }, 2000);
+}
